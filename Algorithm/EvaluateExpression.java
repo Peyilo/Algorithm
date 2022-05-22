41568327fe02a9a8098b1469b288d41ee97464b3
Anvei <103316169+Anvei@users.noreply.github.com>
2022-05-22 12:02:02 +0800

    Add files via upload

--- Algorithm/EvaluateExpression.java
+++ Algorithm/EvaluateExpression.java
@@ -23,17 +23,19 @@ public class EvaluateExpression {
                 while(!operatorStack.isEmpty() && (
                         operatorStack.peek() == '+' ||
                                 operandStack.peek() == '-' ||
-                                operandStack.peek() == '×' || operandStack.peek() == '÷'
+                                operandStack.peek() == '×' || operandStack.peek() == '÷' ||
+                                operandStack.peek() == '*' || operandStack.peek() == '/'
                 )){
                     processAnOperator(operandStack,operatorStack);
                 }
 
                 operatorStack.push(token.charAt(0));
             }
-            else if (token.charAt(0) == '×' || token.charAt(0) == '÷'){
+            else if (token.charAt(0) == '×' || token.charAt(0) == '÷' ||
+                    token.charAt(0) == '*' || token.charAt(0) == '/'){
                 while(!operatorStack.isEmpty() &&
-                        (operatorStack.peek() == '×' ||
-                                operatorStack.peek() == '÷')){
+                        (operatorStack.peek() == '×' || operatorStack.peek() == '*' ||
+                                operatorStack.peek() == '÷' || operatorStack.peek() == '/')){
                     processAnOperator(operandStack,operatorStack);
                 }
                 operatorStack.push(token.charAt(0));
@@ -48,7 +50,7 @@ public class EvaluateExpression {
                 operatorStack.pop();
             }
             else{
-                operandStack.push(new Double(token));
+                operandStack.push(Double.parseDouble(token));
             }
         }
         while(!operatorStack.isEmpty()){
@@ -69,9 +71,9 @@ public class EvaluateExpression {
             operandStack.push(op1 + op2);
         else if (op == '-')
             operandStack.push(op2 - op1);
-        else if (op == '×')
+        else if (op == '×' || op == '*')
             operandStack.push(op2 * op1);
-        else if (op == '÷')
+        else if (op == '÷' || op == '/')
             operandStack.push(op2 / op1);
 
     }
@@ -84,7 +86,8 @@ public class EvaluateExpression {
         for (int i = 0; i< s.length(); i++){
             if (s.charAt(i) == '(' || s.charAt(i) == ')'
                     || s.charAt(i) == '+' || s.charAt(i) == '-'
-                    || s.charAt(i) == '×' || s.charAt(i) == '÷')
+                    || s.charAt(i) == '×' || s.charAt(i) == '÷'
+                    || s.charAt(i) == '*' || s.charAt(i) == '/')
                 result += " " + s.charAt(i) +" ";
             else
                 result += s.charAt(i);
