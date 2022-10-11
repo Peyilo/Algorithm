e05a8d535cb231d10aa788a8e2a90e3d250aacd7
Anvei <103316169+Anvei@users.noreply.github.com>
2022-10-12 00:06:26 +0800

    Add files via upload

---
+++ C/string/util/rpn_expression.h
@@ -0,0 +1,16 @@
+//
+// Created by Anvei on 2022/10/11.
+//
+
+#ifndef ANVEI_RPN_EXPRESSION_H
+#define ANVEI_RPN_EXPRESSION_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+double anvei_rpn_get_result(const char *infix);
+
+#ifdef __cplusplus
+}
+#endif
+#endif //RPN_RPN_EXPRESSION_H
