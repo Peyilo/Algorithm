ef5f815bab523a24d71eb0c69daf19d351b7460f
Anvei <103316169+Anvei@users.noreply.github.com>
2022-10-15 14:13:56 +0800

    Add files via upload

--- C/dataStructure/examples/AVL/basic.c
+++ C/dataStructure/examples/AVL/basic.c
@@ -6,21 +6,22 @@
 #include <stdlib.h>
 #include <time.h>
 
-int get_rand_num()
-{
-    srand(time(NULL));
-    return rand() % 1000;
-}
-
 int main(void)
 {
+    srand(time(NULL));
     AVLTree avl_tree = NULL;
-    for (int i = 0; i < 20; i++)
+    for (int i = 0; i < 6; i++)
     {
-        int num = get_rand_num();
+        int num = i;
         avl_tree = avl_insert(avl_tree, num);
     }
     avl_print_inorder(avl_tree);
+    putchar('\n');
+    avl_tree = avl_delete(avl_tree, 2);
+    avl_tree = avl_delete(avl_tree, 12);
+
+    avl_print_levelorder(avl_tree);
+
     avl_destory(&avl_tree);
     return 0;
 }
